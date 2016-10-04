83d59c93035957562131ae634f34c0b4fbb42168
arunelis <arunas.muraska@gmail.com>
2016-10-04 10:58:25 +0300

    Add validation for hello [name] implementation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Program requires at least 1 argument");
+                return;
+            }
+
+            Console.WriteLine(string.Format("Hello {0}!", args[0]));
 
             Console.ReadLine();
         }
